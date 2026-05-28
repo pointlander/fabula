e0850fe8885e167179b32043b022eb0230c80077
Andrew Snodgrass <pointlander@gmail.com>
2026-05-28 11:12:26 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pointlander/fabula
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,51 @@
+// Copyright 2026 The Fabula Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"embed"
+	"encoding/csv"
+	"fmt"
+	"io"
+)
+
+//go:embed secom.zip
+var Data embed.FS
+
+func main() {
+	file, err := Data.Open("secom.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	reader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		panic(err)
+	}
+	var secom [][]string
+	for _, f := range reader.File {
+		if f.Name == "secom.data" {
+			input, err := f.Open()
+			if err != nil {
+				panic(err)
+			}
+			reader := csv.NewReader(input)
+			secom, err = reader.ReadAll()
+			if err != nil {
+				panic(err)
+			}
+			input.Close()
+		}
+	}
+	fmt.Println(secom)
+}
